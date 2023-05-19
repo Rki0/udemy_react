cd52d65ea8bf0757a4b90b350d63a64ab2f5131b
Rki0 <pky00823@gmail.com>
2023-05-19 23:22:07 +0900

    feat(all): 19.May

--- src/App.js
+++ src/App.js
@@ -1,45 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
 
-const App = () => {
-  const expenses = [
-    {
-      id: "e1",
-      date: new Date(2023, 4, 10),
-      title: "Toilet Paper",
-      amount: "294.67",
-    },
-    {
-      id: "e2",
-      date: new Date(2023, 4, 10),
-      title: "Car Insurance",
-      amount: "124.5",
-    },
-    {
-      id: "e3",
-      date: new Date(2023, 4, 10),
-      title: "Car Insurance",
-      amount: "272",
-    },
-    {
-      id: "e4",
-      date: new Date(2023, 4, 10),
-      title: "Car Insurance",
-      amount: "2.67",
-    },
-    {
-      id: "e5",
-      date: new Date(2023, 4, 10),
-      title: "Car Insurance",
-      amount: "450",
-    },
-  ];
+const DUMMY_EXPENSES = [
+  {
+    id: "e1",
+    date: new Date(2023, 4, 10),
+    title: "Toilet Paper",
+    amount: "294.67",
+  },
+  {
+    id: "e2",
+    date: new Date(2023, 4, 10),
+    title: "Car Insurance",
+    amount: "124.5",
+  },
+  {
+    id: "e3",
+    date: new Date(2023, 4, 10),
+    title: "Car Insurance",
+    amount: "272",
+  },
+  {
+    id: "e4",
+    date: new Date(2023, 4, 10),
+    title: "Car Insurance",
+    amount: "2.67",
+  },
+  {
+    id: "e5",
+    date: new Date(2023, 4, 10),
+    title: "Car Insurance",
+    amount: "450",
+  },
+];
 
-  const addExpenseHandler = (expenses) => {
-    console.log("In App.js");
-    console.log(expenses);
+const App = () => {
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
+
+  const addExpenseHandler = (expense) => {
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses];
+    });
   };
 
   // JSX 코드는 아래 코드와 같은 방식이며, 이를 편하게 사용할 수 있게 해주는데
@@ -56,7 +59,7 @@ const App = () => {
       <h2>Let's get started!</h2>
       <NewExpense onAddExpense={addExpenseHandler} />
 
-      <Expenses expenses={expenses} />
+      <Expenses items={expenses} />
     </div>
   );
 };
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,21 +1,44 @@
+import React, { useState } from "react";
+
 import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
 import Card from "../UI/Card";
-
-import React from "react";
+import ExpensesFilter from "./ExpensesFilter";
 
 function Expenses(props) {
+  const [filteredYear, setFilteredYear] = useState("2020");
+
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
+
+  const filteredExpenses = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+
+  let expensesContent = <p>No expenses found.</p>;
+
   return (
-    <Card className="expenses">
-      {props.expenses.map((item) => (
-        <ExpenseItem
-          key={item.id}
-          date={item.date}
-          title={item.title}
-          amount={item.amount}
+    <div>
+      <Card className="expenses">
+        <ExpensesFilter
+          selected={filteredYear}
+          onChangeFilter={filterChangeHandler}
         />
-      ))}
-    </Card>
+
+        {filteredExpenses.length === 0 && <p>No expenses found.</p>}
+
+        {filteredExpenses.length > 0 &&
+          filteredExpenses.map((expense) => (
+            <ExpenseItem
+              key={expense.id}
+              date={expense.date}
+              title={expense.title}
+              amount={expense.amount}
+            />
+          ))}
+      </Card>
+    </div>
   );
 }
 
